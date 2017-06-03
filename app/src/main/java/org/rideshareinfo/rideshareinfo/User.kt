0d047e862ca6f32698e642692b1a08965b9be855
Rich Friedel <rich.friedel@willowtreeapps.com>
2017-06-03 15:52:32 -0400

    add user class

--- app/src/main/java/org/rideshareinfo/rideshareinfo/User.kt
+++ app/src/main/java/org/rideshareinfo/rideshareinfo/User.kt
@@ -2,5 +2,7 @@ package org.rideshareinfo.rideshareinfo
 
 data class User(
         val name: String,
-        val status: String
+        val status: String,
+        val readyTime: Long,
+        val latLongLocation: Pair<Double, Double>
 )
